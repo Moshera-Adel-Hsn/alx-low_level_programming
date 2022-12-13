f04499274a4d9dffd5d6243dfe831a6e0ab75a90
Moshera-Adel-Hsn <mosherahsn3@gmail.com>
2022-12-12 16:52:06 -0800

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+int main(void)
+{
+char hex;
+for (hex = 0 ; hex <= 'f' ; hex++)
+{
+putchar(hex);
+}
+putchar('\n');
+return (0);
+}
