26f5485dacdf4e27e1dd1c9e143a21ce849027ad
Moshera-Adel-Hsn <mosherahsn3@gmail.com>
2022-12-19 18:46:18 -0800

    AV

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98.
+* @n: An integer input pointe
+* Return: Nothing.
+*/
+void swap_int(int *a, int *b)
+{
+  
+}
