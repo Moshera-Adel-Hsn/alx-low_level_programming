dd0f5247a3cc085440aa836f00964ac0a813d1de
Moshera-Adel-Hsn <mosherahsn3@gmail.com>
2022-12-22 03:11:53 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+int a, b, c, d;
+for (a = '0' ; a <= '9' ; a++)
+{
+for (b = '0' ; b <= '8' ; b++)
+{
+for (c = '0' ; c <= '9' ; c++)
+{
+for (d = '0' ; d <= '9' ; d++)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (a == '9' && b == '8' && c == '9' && d == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
